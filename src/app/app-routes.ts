ab5e9f4ebcd95a6a0bedbf8bbcfbad5f8fde1289
Makoto3110 <cristiancarrillo311087@gmail.com>
2020-06-06 00:04:58 -0400

    se agrega la acción del botón para enrutar a la definición particular a la pag heroe, configurando el router y declarando la función en el app.component.ts

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -2,11 +2,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'heroes', component: HeroesComponent },
+  // para recibir un parámetro, ese será el ID del heroe que yo quiero mostrar, ese será el código cuando pase el código por el url
+  { path: 'heroe/:id', component: HeroeComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
 
