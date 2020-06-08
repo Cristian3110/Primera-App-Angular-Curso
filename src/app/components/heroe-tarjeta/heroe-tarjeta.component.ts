4408f6c5c0990d1d1138ab3334584613c40d7ce9
Makoto3110 <cristiancarrillo311087@gmail.com>
2020-06-07 21:49:23 -0400

    realizamos un output para emitirun evento del hijo hacia el padre

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,12 +8,17 @@ import { Router } from '@angular/router';
 export class HeroeTarjetaComponent implements OnInit {
   @Input() heroe: any = {};
   @Input() index: number;
-  constructor(private router: Router) {}
+  // En el evento emiter, va a emitir el tipo number ya que es el index
+  @Output() heroeSeleccionado: EventEmitter<number>;
+  constructor(private router: Router) {
+    this.heroeSeleccionado = new EventEmitter();
+  }
 
   ngOnInit(): void {}
 
   verHeroe() {
-    console.log(this.index);
-    this.router.navigate(['/heroe', this.index]);
+    // console.log(this.index);
+    // this.router.navigate(['/heroe', this.index]);
+    this.heroeSeleccionado.emit(this.index);
   }
 }
